9b574728fd4797208e56739a5c3317ccd7365fdf
Hariprasath Muthumanickam <39271101+mkhariprasath@users.noreply.github.com>
2022-02-05 00:00:51 +0530

    Update ut_BMS.c

--- ut_BMS.c
+++ ut_BMS.c
@@ -3,11 +3,11 @@
 #include "BatteryTestModule.h"
 #include "ut_BMS.h"
 
-bool isBatteryOKonCondition(float temperature, float chargeState, float chargeRate) 
+void isBatteryOKonCondition(float temperature, float chargeState, float chargeRate) 
 {
  	assert( BATTERY_STATUS IS OK);
 }
-bool isBatteryNOTOKonCondition(float temperature, float chargeState, float chargeRate)  
+void isBatteryNOTOKonCondition(float temperature, float chargeState, float chargeRate)  
 {
 	assert (  BATTERY_STATUS IS_NOT OK);
 }
